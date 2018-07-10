506b3cde17fd5f1e7f76bd9f1f8b3cda77b34f26
Raystar <raystar9@naver.com>
2018-07-10 17:01:23 +0900

    create main branch

---
+++ src/main/java/team/swcome/donong/dto/MainDTO.java
@@ -0,0 +1,22 @@
+package team.swcome.donong.dto;
+
+import javax.annotation.Resource;
+
+@Resource
+public class MainDTO {
+	int id;
+	String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}

4e59aee4a18c8453619dbb3a4332da14383778da
'Raystar' <raystar9@naver.com>
2018-07-12 20:43:11 +0900

    DAO uses template

--- src/main/java/team/swcome/donong/dao/AccountDAO.java
+++ src/main/java/team/swcome/donong/dao/AccountDAO.java
@@ -2,8 +2,9 @@ package team.swcome.donong.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import team.swcome.donong.dto.MainDTO;
@@ -12,9 +13,10 @@ import team.swcome.donong.dto.MainDTO;
 public class AccountDAO {
 	
 	@Autowired
-	SqlSessionFactory factory;
+	@Qualifier("sqlSession")
+	SqlSession session;
 	
 	public List<MainDTO> selectMainList(){
-		return factory.openSession().selectList("Main.selectAll");
+		return session.selectList("Main.selectAll");
 	}
 }

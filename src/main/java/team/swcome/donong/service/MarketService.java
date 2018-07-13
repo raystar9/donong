fe873e7bb419f363e7204d9212c3ccc29e106b7b
'Raystar' <raystar9@naver.com>
2018-07-13 17:31:05 +0900

    changed as mapper interface

--- src/main/java/team/swcome/donong/service/MarketService.java
+++ src/main/java/team/swcome/donong/service/MarketService.java
@@ -5,15 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import team.swcome.donong.dao.MarketDAO;
 import team.swcome.donong.dto.MarketDTO;
 import team.swcome.donong.mapper.BlahMapper;
 
 @Service
 public class MarketService {
 	
-	@Autowired
-	MarketDAO accountDAO;
 	@Autowired
 	BlahMapper mapper;
 	

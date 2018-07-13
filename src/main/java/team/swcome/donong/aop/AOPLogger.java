ee7aa40459704856ff5fc0e9165d3e52d6f33469
Raystar <raystar9@naver.com>
2018-07-13 12:41:05 +0900

    varient changes

--- src/main/java/team/swcome/donong/aop/AOPLogger.java
+++ src/main/java/team/swcome/donong/aop/AOPLogger.java
@@ -22,39 +22,53 @@ public class AOPLogger {
 			+ "or execution(* team.swcome.donong.dao.*.* (..))")
 	public Object LoggerAspect(ProceedingJoinPoint joinPoint) throws Throwable {
 		String targetName = joinPoint.getTarget().getClass().getSimpleName();
-		for (Object arg : joinPoint.getArgs()) {
-			if(arg instanceof List) {
-				for(Object resEach : ((List)arg)) {
+		Object[] args = joinPoint.getArgs();
+
+		if (args.length == 0) {
+			logger.trace("no args ==> {}", targetName);
+		} else {
+			for (Object arg : args) {
+				if (arg instanceof List) {
+					for (Object resEach : ((List) arg)) {
+						StringBuilder sb = new StringBuilder(resEach.getClass().getSimpleName() + " {");
+						Method[] methods = resEach.getClass().getMethods();
+
+						for (Method method : methods) {
+							String methodName = method.getName();
+							if (methodName.startsWith("get") && methodName != "getClass") {
+								sb.append(methodName.substring(3, methodName.length()) + " : " + method.invoke(resEach)
+										+ ", ");
+							}
+						}
+						String logString = sb.substring(0, sb.length() - 2) + "}";
+						logger.trace("{} ==> {}", logString, targetName);
+					}
+				} else {
+					logger.trace("{} ==> {}", arg, targetName);
+				}
+			}
+		}
+		Object res = joinPoint.proceed(args);
+		if (res instanceof List) {
+			@SuppressWarnings("unchecked")
+			List<Object> resArray = (List) res;
+			if (resArray.size() == 0) {
+				logger.trace("empty List <== {}", targetName);
+			} else {
+				for (Object resEach : resArray) {
 					StringBuilder sb = new StringBuilder(resEach.getClass().getSimpleName() + " {");
 					Method[] methods = resEach.getClass().getMethods();
-					
-					for(Method method: methods) {
+
+					for (Method method : methods) {
 						String methodName = method.getName();
-						if(methodName.startsWith("get") && methodName != "getClass") {
-							sb.append(methodName.substring(3, methodName.length()) + " : " +method.invoke(resEach) + ", ");
+						if (methodName.startsWith("get") && methodName != "getClass") {
+							sb.append(methodName.substring(3, methodName.length()) + " : " + method.invoke(resEach)
+									+ ", ");
 						}
 					}
 					String logString = sb.substring(0, sb.length() - 2) + "}";
-					logger.trace("{} ==> {}", logString , targetName);
+					logger.trace("{} <== {}", logString, targetName);
 				}
-			} else {
-				logger.trace("{} ==> {}", arg, targetName);
-			}
-		}
-		Object res = joinPoint.proceed();
-		if(res instanceof List) {
-			for(Object resEach : ((List)res)) {
-				StringBuilder sb = new StringBuilder(resEach.getClass().getSimpleName() + " {");
-				Method[] methods = resEach.getClass().getMethods();
-				
-				for(Method method: methods) {
-					String methodName = method.getName();
-					if(methodName.startsWith("get") && methodName != "getClass") {
-						sb.append(methodName.substring(3, methodName.length()) + " : " +method.invoke(resEach) + ", ");
-					}
-				}
-				String logString = sb.substring(0, sb.length() - 2) + "}";
-				logger.trace("{} <== {}", logString , targetName);
 			}
 		} else {
 			logger.trace("{} <== {}", res, targetName);

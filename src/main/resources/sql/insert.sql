f051e1edcb9d01de6a37f8b2770ab46a0b94689d
Raystar <raystar9@naver.com>
2018-07-23 11:35:54 +0900

    changed insert.sql temp

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -9,14 +9,14 @@ INSERT INTO goods VALUES (1, '모종삽',1000, 3,  '철제 모종삽', '2018-07-
 INSERT INTO goods VALUES (2, '물뿌리개',3000, 3,  '청색 표준 물뿌리개', '2018-07-18', '흔히 볼 수 있는 청색 물뿌리개입니다.', '/donong/resources/image/market/tools/tool2.jpg');
 INSERT INTO goods VALUES (3, '화분(중)',5000, 3,  '300x1200 화분', '2018-07-18', '중형 크기의 화분입니다.', '/donong/resources/image/market/tools/tool3.jpg');
 
-INSERT INTO cart VALUES(1, 1, 1, 2);
-INSERT INTO cart VALUES(2, 2, 1, 1);
+--INSERT INTO cart VALUES(1, 1, 1, 2);
+--INSERT INTO cart VALUES(2, 2, 1, 1);
 INSERT INTO cart VALUES(3, 3, 2, 5);
 
-INSERT INTO orders VALUES(1, 1, 'npay');
-INSERT INTO orders VALUES(2, 1, 'prep');
+INSERT INTO orders VALUES(1, 1, '구명회', '경기도 남양주시 와부읍', '010-9195-0999', '부재시 경비실', 'npay');
+--INSERT INTO orders VALUES(2, 1, 'prep');
 
 INSERT INTO goodsorders VALUES(1, 3, 5);
 INSERT INTO goodsorders VALUES(1, 2, 1);
-INSERT INTO goodsorders VALUES(2, 1, 2);
-INSERT INTO goodsorders VALUES(2, 2, 1);
+--INSERT INTO goodsorders VALUES(2, 1, 2);
+--INSERT INTO goodsorders VALUES(2, 2, 1);

4c30fbabd97e71231b6bd4fe87df6058a2076c7b
Raystar <raystar9@naver.com>
2018-07-25 19:01:41 +0900

    changed orders.date

--- src/main/resources/sql/Create.sql
+++ src/main/resources/sql/Create.sql
@@ -56,7 +56,7 @@ CREATE TABLE orders(
 num NUMBER CONSTRAINT order_num_pk PRIMARY KEY,
 member_num NUMBER CONSTRAINT order_member_num_fk REFERENCES member(num) NOT NULL,
 name VARCHAR2(100) NOT NULL,
-date DATE NOT NULL,
+orderdate DATE NOT NULL,
 address VARCHAR2(200) NOT NULL,
 phone VARCHAR2(15) NOT NULL,
 comments VARCHAR2(300) NOT NULL,
